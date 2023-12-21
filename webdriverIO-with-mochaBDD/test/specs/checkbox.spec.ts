c16600c80c1f168087924c877bb231fcf8bf487f
ramakkonatam <143987445+ramakkonatam@users.noreply.github.com>
2023-12-21 11:04:06 +0530

    Update checkbox.spec.ts

--- webdriverIO-with-mochaBDD/test/specs/checkbox.spec.ts
+++ webdriverIO-with-mochaBDD/test/specs/checkbox.spec.ts
@@ -1,13 +1,13 @@
 import CheckboxPage from '../pageobjects/checkbox.page.ts'
 
 describe('checkboxes', () =>  {
-    it('checkbox 2 should be enabled', async () =>  {
+    it('As a user, I can log into the secure area', async () =>  {
         await CheckboxPage.open()
         await expect(CheckboxPage.firstCheckbox).not.toBeSelected()
         await expect(CheckboxPage.lastCheckbox).toBeSelected()
     })
 
-    it('checkbox 1 should be enabled after clicking on it', async () =>  {
+    it('Sample Feature: Sample Scenario', async () =>  {
         await CheckboxPage.open()
         await expect(CheckboxPage.firstCheckbox).not.toBeSelected()
         await CheckboxPage.firstCheckbox.click()
